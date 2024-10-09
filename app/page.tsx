cff9cf08f66b8b757052c9fa7af8eb47edca52df
Satindar <ibrahimasif711@outlook.com>
2024-10-09 22:44:29 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-black text-white">
+      <main>
+        <h1 className="text-9xl font-black">OpenPages</h1>
+      </main>
+    </div>
+  );
+}
