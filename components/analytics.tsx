5ee9a7329b49d4c043f394d126d8b6afeb31891e
Satindar <ibrahimasif711@outlook.com>
2024-10-30 14:25:56 +0530

    feat: umami

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,9 +1,21 @@
-"use client"
+"use client";
 
+import Script from "next/script";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function Analytics() {
-    return (
-        <GoogleAnalytics strategy="worker" trackPageViews defaultConsent="granted" />
-    )
+  return (
+    <>
+      <GoogleAnalytics
+        strategy="worker"
+        trackPageViews
+        defaultConsent="granted"
+      />
+      <Script
+        src={process.env.NEXT_PUBLIC_UMAMI_URL}
+        data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
